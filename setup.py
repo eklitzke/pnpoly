ff245c0f0cfc47efc0769d3683b623fda15b5e1c
Evan Klitzke <evan@eklitzke.org>
2014-01-20 13:18:44 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name="pnpoly",
-    version="0.0.1",
+    version="0.2",
     ext_modules=[Extension(
         "pnpoly", ["pnpoly.c", "pnpoly_py.c"]
     )]
